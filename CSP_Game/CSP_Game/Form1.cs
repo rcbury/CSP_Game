8e986f8216e6b54cdf69c6bddc835e0b4b20e228
Roma Mikhailovskikh <romanunylov666@gmail.com>
2022-05-06 16:50:52 +0500

    Добавил переход хода

--- CSP_Game/CSP_Game/Form1.cs
+++ CSP_Game/CSP_Game/Form1.cs
@@ -15,6 +15,9 @@ namespace CSP_Game
     public partial class Form1 : Form
     {
         Photo map;
+        int playerIndex;
+        List<Player> players;
+        Player currentPlayer;
 
         public void InitializeMap() 
         {
@@ -28,10 +31,21 @@ namespace CSP_Game
                 }
         }
 
+        public void InitializePlayer() 
+        {
+            players = new List<Player>();
+            players.Add(new Player("Andrew", Color.Green));
+            players.Add(new Player("Roman", Color.Crimson));
+            playerIndex = 0;
+            currentPlayer = players[playerIndex];
+            Text = currentPlayer.Name;
+        }
+
         public Form1()
         {
             InitializeComponent();
             InitializeMap();
+            InitializePlayer();
             pictureBox1.Image = Convertors.Photo2Bitmap(map);
         }
 
@@ -39,8 +53,22 @@ namespace CSP_Game
         {
             int x = (int)Math.Floor((double)Cursor.Position.X / map.pixelWidth);
             int y = (int)Math.Floor((double)(Cursor.Position.Y - 30)/ map.pixelHeight);
-            map[x, y] = new Pixel(0, 1, 0);
+            map[x, y] = new Pixel((double)currentPlayer.Color.R/255, 
+                (double)currentPlayer.Color.G / 255, 
+                (double)currentPlayer.Color.B / 255);
             pictureBox1.Image = Convertors.Photo2Bitmap(map);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (players.Where(player => player.IsAlive).Count() != 1) 
+            {
+                playerIndex++;
+                if (playerIndex > players.Count - 1)
+                    playerIndex = 0;
+                currentPlayer = players[playerIndex];
+                Text = currentPlayer.Name;
+            }
         }
     }
 }
