d0226c4bebf9194d6bc21d2dba763fefb3a3808d
Heanware <45998043+Heanware@users.noreply.github.com>
2022-05-06 17:19:30 +0500

    Correct class names

---
+++ CSP_Game/CSP_Game/Building.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSP_Game
+{
+    public class Building: AnyObject
+    {
+        public readonly Tuple<int, int> Position;
+    }
+
+    public class MiningCamp : Building
+    {
+        public int GPS { get; set; }
+    }
+
+    public class Tower : Building
+    {
+        public double Armor { get; set; }
+    }
+}
