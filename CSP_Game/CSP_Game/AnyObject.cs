b4a09415fb66ce736d57b5aa0bc4135689efc4ee
Heanware <45998043+Heanware@users.noreply.github.com>
2022-05-06 16:28:24 +0500

    Новые классы стандартных объектов и их иерархия

---
+++ CSP_Game/CSP_Game/AnyObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSP_Game
+{
+    public class AnyObject
+    {
+        public int Border { get; set; }
+        public double HP { get; set; }
+        public Color Color { get; set; }
+
+
+
+        public AnyObject()
+        {
+
+        }
+    }
+}
---
+++ CSP_Game/CSP_Game/Unit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSP_Game
+{
+    public interface IMovable
+    {
+        Tuple<int,int> MoveTo();
+    }
+    public class Unit : AnyObject, IMovable
+    {
+        public Tuple<int, int> Position;
+
+        public Tuple<int, int> MoveTo()
+        {
+            throw new Exception();
+        }
+    }
+    public class Tank : Unit
+    {
+
+    }
+}
