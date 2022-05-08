e663094cb50ca3a87e240a55956b8d9cad4e7f13
Heanware <45998043+Heanware@users.noreply.github.com>
2022-05-08 20:21:23 +0500

    Конец работы, проверка бага отрисовки иконок и план дальнейших действий в комментариях к форме

--- CSP_Game/CSP_Game/Form1.Designer.cs
+++ CSP_Game/CSP_Game/Form1.Designer.cs
@@ -144,7 +144,6 @@
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
